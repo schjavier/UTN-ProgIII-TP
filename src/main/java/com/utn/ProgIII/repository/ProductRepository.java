2f4e187742867440e0400df4b6bb6201c18213bf
angeleslibonati <libonatiangeles@gmail.com>
2025-05-22 09:30:52 -0300

    getByName in productRepositori

--- src/main/java/com/utn/ProgIII/repository/ProductRepository.java
+++ src/main/java/com/utn/ProgIII/repository/ProductRepository.java
@@ -2,11 +2,16 @@ package com.utn.ProgIII.repository;
 
 import com.utn.ProgIII.model.Product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-
+    //metodo propio buscar por nombre de producto
+    @Query("SELECT p FROM Product p WHERE p.name like '%:name%'")
+    List<Product> getByname (@Param("name") String name);
+    
     List<Product> findByName (String name);
 }
--- src/main/java/com/utn/ProgIII/service/implementations/ProductServiceImpl.java
+++ src/main/java/com/utn/ProgIII/service/implementations/ProductServiceImpl.java
@@ -49,7 +49,7 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public List<ProductDTO> getProductByName(String name) {
-        List<Product> products = productRepository.findByName(name);
+        List<Product> products = productRepository.getByname(name);
         List<ProductDTO> productDTOS = new ArrayList<>();
 
         for(Product product:products){
