b43f0b734183846a189288f63d12795301343fdc
Tomas Silva <toto9116@gmail.com>
2025-05-23 20:54:05 -0300

    (improve) address validations

--- src/main/java/com/utn/ProgIII/model/Address/Address.java
+++ src/main/java/com/utn/ProgIII/model/Address/Address.java
@@ -1,6 +1,5 @@
 package com.utn.ProgIII.model.Address;
 
-import com.utn.ProgIII.model.Supplier.Supplier;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -17,15 +16,22 @@ public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long idAddress;
-    @NotBlank(message = "La calle no puede estar vacia!")
-    @Length(min = 3, max = 50, message = "El largo de la calle no es correcto")
+
+    @NotBlank(message = "El nombre de la calle no puede estar vacio")
+    @Length(min = 3, max = 50, message = "El nombre de la calle debe contener entre 3 y 50 letras")
+    @Pattern(regexp = "^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ\\s]+$",
+            message = "El nombre de la calle solo puede contener letras, espacios, y numeros")
     private String street;
-    @NotBlank(message = "El numero de calle no puede estar vacio!")
-    @Length(min = 2, max = 5, message = "El largo de la calle no es correcto")
-    @Pattern(regexp = "\\d+", message = "El numero no tiene solo numeros!")
+
+    @NotBlank(message = "La altura de la calle no puede estar vacia")
+    @Length(min = 1, max = 5, message = "La altura de la calle debe contener entre 1 y 5 numeros ")
+    @Pattern(regexp = "\\d+", message = "La altura de la calle tiene caracteres no numericos")
     private String number;
-    @NotBlank(message = "El nombre de la ciudad no puede ser vacio!")
-    @Length(message = "El largo no es correcto",min = 3, max = 20)
+
+    @NotBlank(message = "El nombre de la ciudad no puede estar vacio")
+    @Length(min = 3, max = 50, message = "El nombre de la ciudad debe contener entre 3 y 50 letras")
+    @Pattern(regexp = "^[a-zA-ZáéíóúÁÉÍÓÚñÑ\\s]+$",
+            message = "El nombre de la ciudad solo puede contener letras y espacios")
     private String city;
 
     public Address(String city, String number, String street) {
