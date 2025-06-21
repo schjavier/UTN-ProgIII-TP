ca84ee26b9bbe3bbab6123f96535416308723b08
Javier <schjavier@gmail.com>
2025-06-21 13:18:58 -0300

    add: more tests for productService

--- src/test/java/com/utn/ProgIII/serviceTest/ProductServiceTest.java
+++ src/test/java/com/utn/ProgIII/serviceTest/ProductServiceTest.java
@@ -1,6 +1,7 @@
 package com.utn.ProgIII.serviceTest;
 
 import com.utn.ProgIII.dto.ProductDTO;
+import com.utn.ProgIII.exceptions.DuplicateEntityException;
 import com.utn.ProgIII.exceptions.InvalidProductStatusException;
 import com.utn.ProgIII.exceptions.ProductNotFoundException;
 import com.utn.ProgIII.mapper.ProductMapper;
@@ -8,15 +9,14 @@ import com.utn.ProgIII.model.Product.Product;
 import com.utn.ProgIII.model.Product.ProductStatus;
 import com.utn.ProgIII.repository.ProductRepository;
 import com.utn.ProgIII.repository.ProductSupplierRepository;
-import com.utn.ProgIII.service.implementations.AuthServiceImpl;
 import com.utn.ProgIII.service.implementations.ProductServiceImpl;
 import com.utn.ProgIII.service.interfaces.AuthService;
-import com.utn.ProgIII.service.interfaces.ProductService;
 import com.utn.ProgIII.validations.ProductValidations;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -50,65 +50,61 @@ public class ProductServiceTest {
     private static final String PRODUCT_NAME = "product One";
     private static final Long PRODUCT2_ID = 3L;
     private static final String PRODUCT2_NAME = "One product";
-    private static final ProductStatus STATUS = ProductStatus.ENABLED;
+    private static final ProductStatus STATUS_ENABLED = ProductStatus.ENABLED;
     private static final ProductStatus STATUS_DISABLED = ProductStatus.DISABLED;
     private static final String INVALID_STATUS = "Enbled";
     private static final Long NON_EXISTING_ID = 2L;
 
-    private Product productMock;
-    private Product disabledProductMock;
-    private Product savedProduct;
-    private ProductDTO productDTOMock;
-    private ProductDTO productDTOMock2;
-    private List<Product> productList;
-    private List<Product> onlyEnabledProductList;
+    private Product product;
+    private ProductDTO productDTO;
 
     @BeforeEach
     void setUp(){
 
-        productMock = new Product();
-        productMock.setIdProduct(PRODUCT_ID);
-        productMock.setName(PRODUCT_NAME);
-        productMock.setStatus(STATUS);
+        product = createBasicEnabledProduct();
+        productDTO = createBasicProductDto();
 
-        savedProduct = new Product();
-        savedProduct.setIdProduct(PRODUCT_ID);
-        savedProduct.setName(PRODUCT_NAME);
-        savedProduct.setStatus(STATUS);
+    }
 
-        disabledProductMock = new Product();
-        disabledProductMock.setIdProduct(PRODUCT_ID);
-        disabledProductMock.setName(PRODUCT2_NAME);
-        disabledProductMock.setStatus(STATUS_DISABLED);
+    private Product createBasicEnabledProduct(){
+        Product product = new Product();
+        product.setIdProduct(PRODUCT_ID);
+        product.setName(PRODUCT_NAME);
+        product.setStatus(STATUS_ENABLED);
 
-        productDTOMock = new ProductDTO(PRODUCT_ID, PRODUCT_NAME, STATUS.toString());
-        productDTOMock2 = new ProductDTO(PRODUCT2_ID, PRODUCT2_NAME, STATUS_DISABLED.toString());
+        return product;
+    }
 
-        productList = new ArrayList<>();
-        productList.add(productMock);
-        productList.add(disabledProductMock);
+    private Product createDisabledProduct(){
+        Product product = createBasicEnabledProduct();
+        product.setName(PRODUCT2_NAME);
+        product.setStatus(STATUS_DISABLED);
+        return product;
+    }
 
-        onlyEnabledProductList = new ArrayList<>();
-        onlyEnabledProductList.add(productMock);
+    private ProductDTO createBasicProductDto(){
+        return new ProductDTO(PRODUCT_ID, PRODUCT_NAME, STATUS_ENABLED.toString());
+    }
 
-
+    private ProductDTO createBasicDisabledProductDto(){
+        return new ProductDTO(PRODUCT2_ID, PRODUCT2_NAME, STATUS_DISABLED.toString());
     }
 
     @Test
     public void getProductById_shouldReturnProductDTO_whenProductExists(){
 
-        when(productRepository.findById(PRODUCT_ID)).thenReturn(Optional.of(productMock));
-        when(productMapper.toProductDTO(productMock)).thenReturn(productDTOMock);
+        when(productRepository.findById(PRODUCT_ID)).thenReturn(Optional.of(product));
+        when(productMapper.toProductDTO(product)).thenReturn(productDTO);
 
         ProductDTO result = productService.getProductById(PRODUCT_ID);
 
         Assertions.assertNotNull(result);
         Assertions.assertEquals(result.idProduct(), PRODUCT_ID);
         Assertions.assertEquals(result.name(), PRODUCT_NAME);
-        Assertions.assertEquals(result.status(), STATUS.toString());
+        Assertions.assertEquals(result.status(), STATUS_ENABLED.toString());
 
         Mockito.verify(productRepository).findById(PRODUCT_ID);
-        Mockito.verify(productMapper).toProductDTO(productMock);
+        Mockito.verify(productMapper).toProductDTO(product);
 
     }
 
@@ -126,7 +122,9 @@ public class ProductServiceTest {
     @Test
     public void getUserBy_shouldThrowException_whenUserIsEmployeeAndProductIsDisabled(){
 
-        when(productRepository.findById(PRODUCT_ID)).thenReturn(Optional.of(disabledProductMock));
+        Product disabledProduct = createDisabledProduct();
+
+        when(productRepository.findById(PRODUCT_ID)).thenReturn(Optional.of(disabledProduct));
         when(authService.isEmployee()).thenReturn(true);
 
         assertThrows(ProductNotFoundException.class,
@@ -140,30 +138,39 @@ public class ProductServiceTest {
 
     @Test
     void getUserById_shouldReturnProduct_whenEmployeeAndProductEnabled(){
-        when(productRepository.findById(PRODUCT_ID)).thenReturn(Optional.of(productMock));
+
+
+        when(productRepository.findById(PRODUCT_ID)).thenReturn(Optional.of(product));
         when(authService.isEmployee()).thenReturn(true);
-        when(productMapper.toProductDTO(productMock)).thenReturn(productDTOMock);
+        when(productMapper.toProductDTO(product)).thenReturn(productDTO);
 
         ProductDTO result = productService.getProductById(PRODUCT_ID);
 
-        assertEquals(productDTOMock, result);
-        verify(productMapper).toProductDTO(productMock);
+        assertEquals(productDTO, result);
+        verify(productMapper).toProductDTO(product);
 
     }
 
     @Test
     void getAllProducts_shouldReturnListOfAllProducts_whenUserManager(){
+        Product disabledProduct = createDisabledProduct();
+        ProductDTO disableProductDto = createBasicDisabledProductDto();
+
+        List<Product> productList = new ArrayList<>();
+        productList.add(product);
+        productList.add(disabledProduct);
+
 
         when(productRepository.findAll()).thenAnswer(
                 invocation -> productList
         );
-        when(productMapper.toProductDTO(productMock)).thenReturn(productDTOMock);
-        when(productMapper.toProductDTO(disabledProductMock)).thenReturn(productDTOMock2);
+        when(productMapper.toProductDTO(product)).thenReturn(productDTO);
+        when(productMapper.toProductDTO(disabledProduct)).thenReturn(disableProductDto);
 
         List<ProductDTO> result = productService.getAllProduct();
 
         assertEquals(2, result.size());
-        assertTrue(result.containsAll(List.of(productDTOMock, productDTOMock2)));
+        assertTrue(result.containsAll(List.of(productDTO, disableProductDto)));
         verify(productRepository).findAll();
         verify(productRepository, never()).findByStatus(any());
     }
@@ -171,18 +178,21 @@ public class ProductServiceTest {
     @Test
     void getAllProducts_shouldReturnOnlyEnabledProducts_whenUserEmployee(){
 
+        List<Product> onlyEnabledProductList = new ArrayList<>();
+        onlyEnabledProductList.add(product);
+
         when(authService.isEmployee()).thenReturn(true);
-        when(productRepository.findByStatus(STATUS)).thenAnswer(
+        when(productRepository.findByStatus(STATUS_ENABLED)).thenAnswer(
                 invocation -> onlyEnabledProductList
         );
 
-        when(productMapper.toProductDTO(productMock)).thenReturn(productDTOMock);
+        when(productMapper.toProductDTO(product)).thenReturn(productDTO);
 
         List<ProductDTO> result = productService.getAllProduct();
 
         assertEquals(1, result.size());
-        assertTrue(result.contains(productDTOMock));
-        verify(productRepository).findByStatus(STATUS);
+        assertTrue(result.contains(productDTO));
+        verify(productRepository).findByStatus(STATUS_ENABLED);
 
 
     }
@@ -190,16 +200,19 @@ public class ProductServiceTest {
     @Test
     void getAllProductsByState_shouldReturnListOfSameStateProducts(){
 
-        when(productRepository.findByStatus(STATUS)).thenAnswer(
+        List<Product> onlyEnabledProductList = new ArrayList<>();
+        onlyEnabledProductList.add(product);
+
+        when(productRepository.findByStatus(STATUS_ENABLED)).thenAnswer(
                 invocationOnMock -> onlyEnabledProductList
         );
-        when(productMapper.toProductDTO(productMock)).thenReturn(productDTOMock);
+        when(productMapper.toProductDTO(product)).thenReturn(productDTO);
 
-        List<ProductDTO> result = productService.getAllProductByStatus(STATUS.toString());
+        List<ProductDTO> result = productService.getAllProductByStatus(STATUS_ENABLED.toString());
 
         assertNotNull(result);
         assertEquals(1, result.size());
-        assertTrue(result.contains(productDTOMock));
+        assertTrue(result.contains(productDTO));
 
     }
 
@@ -219,11 +232,14 @@ public class ProductServiceTest {
     @Test
     void getProductByName_shouldReturnAListOfEnabledProducts_whenUserEmployee(){
 
+        List<Product> onlyEnabledProductList = new ArrayList<>();
+        onlyEnabledProductList.add(product);
+
         when(productRepository.findByNameContaining("One")).thenAnswer(
                 invocationOnMock -> onlyEnabledProductList
         );
         when(authService.isEmployee()).thenReturn(true);
-        when(productMapper.toProductDTO(productMock)).thenReturn(productDTOMock);
+        when(productMapper.toProductDTO(product)).thenReturn(productDTO);
 
         List<ProductDTO> result = productService.getProductByName("One");
 
@@ -231,18 +247,25 @@ public class ProductServiceTest {
         assertEquals(result.get(0).name(), "product One");
         assertEquals(result.get(0).status(), "ENABLED");
         verify(productRepository).findByNameContaining("One");
-        verify(productMapper).toProductDTO(productMock);
+        verify(productMapper).toProductDTO(product);
     }
 
     @Test
     void getProductByName_shouldReturnAListOfAllProducts_whenUserManager(){
 
+        Product disabledProduct = createDisabledProduct();
+        ProductDTO disabledProductDTO = createBasicDisabledProductDto();
+
+        List<Product> productList = new ArrayList<>();
+        productList.add(product);
+        productList.add(disabledProduct);
+
         when(productRepository.findByNameContaining("One")).thenAnswer(
                 invocationOnMock -> productList
         );
         when(authService.isEmployee()).thenReturn(false);
-        when(productMapper.toProductDTO(productMock)).thenReturn(productDTOMock);
-        when(productMapper.toProductDTO(disabledProductMock)).thenReturn(productDTOMock2);
+        when(productMapper.toProductDTO(product)).thenReturn(productDTO);
+        when(productMapper.toProductDTO(disabledProduct)).thenReturn(disabledProductDTO);
 
         List<ProductDTO> result = productService.getProductByName("One");
 
@@ -252,8 +275,80 @@ public class ProductServiceTest {
         assertEquals(result.get(1).name(), "One product");
         assertEquals(result.get(1).status(), "DISABLED");
         verify(productRepository).findByNameContaining("One");
-        verify(productMapper).toProductDTO(productMock);
+        verify(productMapper).toProductDTO(product);
     }
+
+    @Test
+    void createProductDto_shouldReturnACreatedProduct_whenValidInput(){
+
+       ProductDTO inputDTO = new ProductDTO(null, PRODUCT_NAME, STATUS_ENABLED.toString());
+       Product nullIdProduct = createBasicEnabledProduct();
+       nullIdProduct.setIdProduct(null);
+
+        when(productMapper.toEntity(inputDTO)).thenReturn(nullIdProduct);
+        when(productRepository.save(nullIdProduct)).thenReturn(product);
+        when(productMapper.toProductDTO(product)).thenReturn(productDTO);
+
+        ProductDTO result = productService.createProductDto(inputDTO);
+
+        assertNotNull(result);
+        assertEquals(1L, result.idProduct());
+        assertEquals(PRODUCT_NAME, result.name());
+
+        verify(productValidations).validateProductNameExists(nullIdProduct);
+        verify(productMapper).toEntity(inputDTO);
+        verify(productRepository).save(nullIdProduct);
+        verify(productMapper).toProductDTO(product);
+
+    }
+
+    @Test
+    void createProductDTO_shouldThrowAnException_whenProductAlreadyExist(){
+
+        when(productMapper.toEntity(productDTO)).thenReturn(product);
+        doThrow(new DuplicateEntityException("El producto ya existe"))
+                .when(productValidations).validateProductNameExists(product);
+
+        assertThrows(DuplicateEntityException.class,
+                () -> productService.createProductDto(productDTO));
+
+        verify(productValidations).validateProductNameExists(product);
+        verify(productRepository, never()).save(any());
+        verify(productMapper, never()).toProductDTO(any());
+
+    }
+
+    @Test
+    void updateProduct_shouldReturnUpdatedProduct_whenValidInput(){
+
+        Product initialProduct = createBasicEnabledProduct();
+        initialProduct.setStatus(STATUS_DISABLED);
+
+        ProductDTO updateDto = new ProductDTO(PRODUCT_ID, "product Two", STATUS_ENABLED.toString());
+
+        when(productRepository.findById(PRODUCT_ID)).thenReturn(Optional.of(initialProduct));
+        when(productRepository.save(any(Product.class)))
+                .thenAnswer( invocationOnMock -> invocationOnMock.getArgument(0) );
+
+        when(productMapper.toProductDTO(any(Product.class))).thenReturn(updateDto);
+
+        ProductDTO result = productService.updateProduct(PRODUCT_ID, updateDto);
+
+        assertEquals("product Two", result.name());
+        assertEquals("ENABLED", result.status());
+
+        verify(productRepository).findById(PRODUCT_ID);
+        verify(productRepository).save(any(Product.class));
+        ArgumentCaptor<Product> productArgumentCaptor = ArgumentCaptor.forClass(Product.class);
+        verify(productMapper).toProductDTO(productArgumentCaptor.capture());
+
+        Product savedProduct = productArgumentCaptor.getValue();
+        assertEquals("product Two", savedProduct.getName());
+        assertEquals(ProductStatus.ENABLED, savedProduct.getStatus());
+
+    }
+
+    // Faltan mas tests pero estoy cansado jefe!
 
 
 }
